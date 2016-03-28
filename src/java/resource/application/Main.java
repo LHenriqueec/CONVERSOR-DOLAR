0ab0151a404531350d4f7df523b7b5f30d74de3b
LuizS <luizsnip@gmail.com>
2016-03-28 12:36:27 -0300

    Implementação da View principal

--- src/java/resource/application/Main.java
+++ src/java/resource/application/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 		Scene scene = new Scene(root);
 		
 		stage.setTitle("CONVERSOR DOLAR");
+		stage.setResizable(false);
 		stage.setScene(scene);
 		stage.show();
 	}
